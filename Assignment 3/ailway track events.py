4dcdb271b0e5ac4758960a42dae2a0dbbd5f765b
Daso-2020 <adam.albrejawi.00@gmail.com>
2026-02-22 19:54:09 +0100

    Loading the three datasets

---
+++ Assignment 3/ailway track events.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# ---  Loading the three datasets ---
+file1 = "Trail1_extracted_features_acceleration_m1ai1-1.csv"
+file2 = "Trail2_extracted_features_acceleration_m1ai1.csv"
+file3 = "Trail3_extracted_features_acceleration_m2ai0.csv"
+
+df1 = pd.read_csv(file1)
+df2 = pd.read_csv(file2)
+df3 = pd.read_csv(file3)
+
+print("Trail1 shape:", df1.shape)
+print("Trail2 shape:", df2.shape)
+print("Trail3 shape:", df3.shape)
+
+print("\nColumns preview:\n", df1.columns)
+print("\nEvent values example:\n", df1["event"].value_counts().head(10))
